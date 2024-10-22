11cc0891627f085a30d5a06c8d91aa6cc7311fd3
Stuart Barlow <sbarlster@gmail.com>
2024-10-22 22:43:27 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,52 @@
+Password Generator App
+======================
+
+This is a simple command line program that will either generate a random password or
+check that a user supplied password conforms to a set of rules.
+
+
+Rules
+-----
+
+* Mimimum length of password is 12 chartacters
+* Must contain a mixture of lowercase, uppercase, special characters and numbers
+
+
+Usage
+-----
+
+```
+password-generator <user supplied password> -r <FLAG to generate a random password>
+```
+
+
+Articles that helped
+====================
+
+command line app:
+https://pypi.org/project/plac/
+
+importing modules
+https://www.freecodecamp.org/news/python-requirementstxt-explained/
+
+converting lists into a single string:
+https://www.freecodecamp.org/news/python-list-to-string-how-to-convert-lists-in-python/
+
+python constants
+https://realpython.com/python-constants/#what-constants-are
+
+creating mixed random python strings
+https://stackoverflow.com/questions/2257441/random-string-generation-with-upper-case-letters-and-digits
+
+python random
+https://docs.python.org/3/library/random.html#random.choice
+
+naming conventions
+https://peps.python.org/pep-0008/#function-and-method-arguments
+
+using a set to check subset of valid characters
+https://www.tutorialspoint.com/how-to-check-if-a-string-only-contains-certain-characters-in-python
+
+does a set contain an element
+https://www.geeksforgeeks.org/how-to-check-if-a-set-contains-an-element-in-python/
+
---
+++ password-generator.py
@@ -0,0 +1,56 @@
+import string
+import plac
+import random
+
+MINIMUM_LENGTH = 12
+
+def check_password_rules(password):
+    if (len(password) < MINIMUM_LENGTH):
+        raise RuntimeError('password too short, must be at least {} characters long'.format(MINIMUM_LENGTH))
+    
+    # must contain only
+    valid_chars = set(string.ascii_lowercase + string.ascii_uppercase + string.punctuation + string.digits)
+    password_set = set(password)
+    if not (password_set.issubset(valid_chars)):
+        raise RuntimeError('password contains an invalid character, password is: {}'.format(password))
+    
+    # must have at least one of each of the categories
+    
+
+def check_password_unique(password):
+    # load previous passwords from file
+    with open("password-file.txt", "r+") as f:
+        previous_pws = set(f.read().split())
+        print(previous_pws)
+        # check new password is not in set
+        if (password in previous_pws):
+            raise RuntimeError('password {} has been used before'.format(password))
+
+    # write the new password to the file
+    with open('password-file.txt', 'a') as file:
+        file.write(password + '\n')
+
+
+@plac.opt('password', help="the user generate password - will be checked against the rules")
+@plac.flg('randompw', help="if set then a random password will be generated")
+def main(password, randompw = False) :
+    """
+    A program to generate passwords - default behaviour is to request a password from the user
+
+    """
+
+    if (randompw):
+        pw_characters = random.choices(string.ascii_lowercase + string.ascii_uppercase + string.digits + string.punctuation,
+                                       k=MINIMUM_LENGTH)
+        password = ''.join(pw_characters)
+    
+    check_password_rules(password)
+
+    check_password_unique(password)
+
+    print('your password is: {}'.format(password))
+
+
+
+if __name__ == '__main__':
+    plac.call(main)
